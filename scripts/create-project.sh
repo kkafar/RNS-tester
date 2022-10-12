eda55a9da543dd876df6c60ad1dc19e54ae47c52
Kacper Kafara <kacperkafara@gmail.com>
2022-10-12 19:48:52 +0200

    Add call to create_rns_app func

--- scripts/create-project.sh
+++ scripts/create-project.sh
@@ -12,12 +12,17 @@ create_rns_app() {
   cd ${RNST_ROOT_DIR}
 
   npx react-native init "$1" --version "$2"
+
+  cd "${RNST_ROOT_DIR}/$1"
+
   yarn add react-native-screens
   yarn add react-native-safe-area-context
   yarn add @react-navigation/native-stack
   yarn add @react-navigation/native
 
   cp "${RNST_ROOT_DIR}/scripts/App.template.js" "${RNST_ROOT_DIR}/$1/App.js"
+  
+  cd ${RNST_ROOT_DIR}
 }
 
 assert_cmd_exists "npx"
@@ -26,3 +31,5 @@ assert_cmd_exists "yarn"
 # $1 - app name
 # $2 - RN version
 assert_arg_count 2 $#
+
+create_rns_app $1 $2
