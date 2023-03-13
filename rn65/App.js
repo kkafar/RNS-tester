d31b1874b5c7c6136c2e954ea49ab63e8f6834d4
Kacper Kafara <kacperkafara@gmail.com>
2023-03-13 16:40:56 +0100

    Add all projects

---
+++ rn65/App.js
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { Text } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+export function Home() {
+  return <Text>Hello world!</Text>;
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
