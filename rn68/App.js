b4a7ac79bc7f53370ff7db92fe7f1a846f032784
Kacper Kafara <kacperkafara@gmail.com>
2023-03-13 16:42:41 +0100

    Add project (stash all changes) (#3)
    * Disable hermes
    
    * Add dependency on patch-package
    
    * Add patch for cli-platform-android
    
    * Install 3.17.0
    
    * Add all projects

--- rn68/App.js
+++ rn68/App.js
@@ -1,10 +1,65 @@
-import { NavigationContainer } from '@react-navigation/native';
+/* eslint-disable react-hooks/exhaustive-deps */
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import { Text } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {Button, ScrollView, Text} from 'react-native';
+import {createNativeStackNavigator} from 'react-native-screens/native-stack';
 
-export function Home() {
-  return <Text>Hello world!</Text>;
+export function Home({navigation}) {
+  const sbRef = React.useRef(null);
+  const [search, setSearch] = React.useState('');
+
+  const searchBarOptions = {
+    // @ts-ignore
+    ref: sbRef,
+    barTintColor: 'powderblue',
+    hideWhenScrolling: true,
+    obscureBackground: false,
+    hideNavigationBar: false,
+    autoCapitalize: 'sentences',
+    placeholder: 'Some text',
+    onChangeText: e => setSearch(e.nativeEvent.text),
+    onCancelButtonPress: () => console.warn('Cancel button pressed'),
+    onSearchButtonPress: () => console.warn('Search button pressed'),
+    onFocus: () => console.warn('onFocus event'),
+    onBlur: () => console.warn('onBlur event'),
+  };
+
+  React.useEffect(() => {
+    navigation.setOptions({
+      searchBar: searchBarOptions,
+    });
+  }, [navigation]);
+
+  return (
+    <ScrollView>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Text>Some text</Text>
+      <Button title="Focus search bar" onPress={() => sbRef.current.focus()} />
+      <Button title="Blur search bar" onPress={() => sbRef.current.blur()} />
+    </ScrollView>
+  );
 }
 
 const Stack = createNativeStackNavigator();
@@ -12,7 +67,11 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerLargeTitle: true,
+          headerTranslucent: true,
+        }}>
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
